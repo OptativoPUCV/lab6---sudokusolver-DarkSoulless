2f693b8cdb4ec304629b5460eb7dafe165d82892
Replit user <>
2023-05-07 03:44:18 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -93,28 +93,26 @@ int is_valid(Node* n){
 
 
 
-List* get_adj_nodes(Node* n) {
-    List* list = createList();
-    
-    for (int i = 0; i < 9; i++) {
-        for (int j = 0; j < 9; j++) {
-            if (n->sudo[i][j] == 0) {
-                for (int k = 1; k < 10; k++) {
-                    Node* nuevo = createNode();
-                    *nuevo = *n;
-                    nuevo->sudo[i][j] = k;
-                    if (is_valid(nuevo)) {
-                        pushBack(list, nuevo);
-                    } else {
-                        free(nuevo);
-                    }
-                }
-                return list;
-            }
+List* get_adj_nodes(Node* n){
+  List* list = createList();
+  for (int i = 0; i < 9; i++){
+    for (int j = 0; j < 9; j++){
+      if (n->sudo[i][j] == 0){
+        for (int k = 1; k < 10; k++){
+          Node* nuevo = createNode();
+          *nuevo = *n;
+          nuevo->sudo[i][j] = k;
+          if (is_valid(nuevo)){
+            pushBack(list, nuevo);
+          } else {
+            free(nuevo);
+          }
         }
+        return list;
+      }
     }
-    
-    return list;
+  } 
+  return list;
 }
 
 
@@ -131,7 +129,7 @@ int is_final(Node* n){
 
 
 Node* DFS(Node* initial, int* cont){
-  return NULL;
+  Stack
 }
 
 
